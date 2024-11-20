bbfe7df3771b6ca827a16cb214ccdbc1499c7933
linyu-ch <linyuenhong@gmail.com>
2024-11-20 14:13:46 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Hello, this is a cypress testing framework demo, our project is built using vite and react</h1>
+      </header>
+
+      <section id='section-1'>
+        <h2>test demo 1</h2>
+        <p>Button function test</p>
+        </section>
+      <section id='section-2'>
+        <h2>test demo 2</h2>
+        <p>Form submission test</p>
+        
+        </section>
+      <section id='section-3'>
+        <h2>test demo 3</h2>
+        <p>Link navigation test</p>
+
+        </section>
+      <section id='section-4'>
+        <h2>test demo 4</h2>
+        <p>Components rendering test</p>
+        
+        </section>
+      <section id='section-5'>
+        <h2>test demo 5</h2>
+        <p>List rendering test</p>
+        
+        </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/componentForTesting.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TestComponent = ()=>{
+    return (
+        <div>
+            this is an incomplete test component
+        </div>
+    );
+}
+
+export default TestComponent;
